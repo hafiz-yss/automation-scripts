ce3d9d5b4ed202305a6877349e203a26ed5eb2e3
Hafiz Stores <yakubhafiz17@gmail.com>
2025-07-17 22:02:52 +0100

    feat: find files matching series name

--- sort-episodes/main.py
+++ sort-episodes/main.py
@@ -6,3 +6,14 @@ series_name = input("Enter the TV series name: ")
 target_folder = os.path.expanduser(f"~/videos/{series_name}")
 
 print(f"Moving files from {downloads_folder} to {target_folder}")
+
+found_any = False
+
+for file in os.listdir(downloads_folder):
+    if series_name.lower() in file.lower():
+        print(f"Found: {file}")
+        found_any = True
+
+if not found_any:
+    print("No matching file found")
+    exit()
